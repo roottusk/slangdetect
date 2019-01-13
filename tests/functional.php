bcdf50ed134179365d99bc52ba669fe0eb7b1452
Tushar Kulkarni <root.tusk@gmail.com>
2019-01-13 14:58:45 +0530

    Create functional.php

---
+++ tests/functional.php
@@ -0,0 +1,23 @@
+<?php
+
+require("../lib/init.php");
+
+$strTEST=' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+
+CheckAll();
+
+function CheckAll()
+{
+global $strTEST;
+
+
+ContainsBadWord($strTEST);
+IsBadWord($strTEST);
+StripBadWords($strTEST);
+StripOneWord($strTEST,"ABCD");
+exit(1);
+
+
+}
+
+?>
